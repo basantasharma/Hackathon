ff48049a2423269a02cd3cd34d83e9967bea973a
basantasharma <gyawalibasanta0@gmail.com>
2021-07-08 20:45:27 +0545

    Post view and post add included

---
+++ App/Controllers/Post.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Auth;
+use App\Flash;
+use App\Models\User;
+use App\Models\UserPost;
+use Core\View;
+
+
+class Post extends \Core\Controller
+{
+
+    public function addAction()
+    {
+        $this->requireLogin();
+        
+        View::renderTemplate('post/new.html');
+        
+    }
+
+    public function createAction()
+    {
+        $userPost = new UserPost($_POST);
+        if ($userPost->savePost()) 
+        {
+            $this->redirect('/'); 
+        }
+        else
+        {
+            View::renderTemplate('post/new.html', [
+                'userPost' => $userPost
+            ]);
+        }   
+    }
+
+    public function showAction()
+    {
+        $this->postModel = new UserPost();
+        $postDetails = $this->postModel::getPost();
+        View::renderTemplate('post/index.html',[
+            'postDetails' => $postDetails
+        ]);
+
+        //var_dump($postDetails);
+        
+    }
+
+    public function emergencyAction()
+    {
+        $this->postModel = new UserPost();
+        $postDetails = $this->postModel::getPost();
+        View::renderTemplate('post/emergency.html',[
+            'postDetails' => $postDetails
+        ]);
+
+        //var_dump($postDetails);
+        
+    }
+
+    public function generalAction()
+    {
+        $this->requireLogin();
+        $this->postModel = new UserPost();
+        $postDetails = $this->postModel::getPost();
+        View::renderTemplate('post/general.html',[
+            'postDetails' => $postDetails
+        ]);
+
+        //var_dump($postDetails);
+        
+    }
+
+    public function EditAction()
+    {
+        $this->requireLogin();
+        //$user = Auth::getUser();
+        if(isset($_GET['post_id']) && UserPost::isUserPost($_GET['post_id']))
+        {
+            View::renderTemplate('post/edit.html',[
+                'post_details' => UserPost::getUserPostById($_GET['post_id'])
+            ]);        
+        }
+        else
+        {
+            // $user = Auth::getUser();
+            View::renderTemplate('/404.html');
+        }
+    }
+
+    public function StartEditAction()
+    {
+        $this->requireLogin();
+        $edit = UserPost::updatepost($_POST, $_GET['post_id']);
+        if($edit)
+        {
+            $this->redirect('/post/show');
+        }        
+    }
+
+    public function deletePostAction()
+    {
+        $this->requireLogin();
+        $post_id = $_GET['post_id'];
+        //$user = Auth::getUser();
+        if(UserPost::isUserPost($post_id))
+        {
+            UserPost::deletePost($post_id);
+            $this->redirect('/post/show');      
+        }
+        else
+        {
+            $this->redirect('/post/show');
+        }
+
+    }
+
+}
---
+++ App/Models/UserPost.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Models;
+
+use App\Auth;
+use PDO;
+use \App\Token;
+use \Core\View;
+
+/**
+ * User model
+ *
+ * PHP version 7.0
+ */
+class UserPost extends \Core\Model
+{
+    /**
+     * Error messages
+     *
+     * @var array
+     */
+    public $errorsPost = [];
+
+    /**
+     * Class constructor
+     *
+     * @param array $data  Initial property values (optional)
+     *
+     * @return void
+     */
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        };
+    }
+
+    public function savePost()
+    {
+        $user = Auth::getUser();
+        $this->validatePost();
+        $date = date("Y-m-d H:i:s");
+
+        if (empty($this->errorsPost)) 
+        {
+            $sql = 'INSERT INTO user_posts (user_id, title, catogery, discription, date) 
+            VALUES(:user_id, :post_title, :post_type, :post_discription, :post_date)';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':user_id', $user->id, PDO::PARAM_INT);
+            $stmt->bindValue(':post_title', $this->title, PDO::PARAM_STR);
+            $stmt->bindValue(':post_type', $this->type, PDO::PARAM_STR);
+            $stmt->bindValue(':post_discription', $this->details, PDO::PARAM_STR);
+            $stmt->bindValue(':post_date', $date, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+        return false;
+    }
+
+    public function validatePost()
+    {
+        //validate post title
+        if ($this->title == "") {
+            $this->errorsPost[] = 'Title is required';
+        }
+
+        //validate discription
+        if ($this->details == "") {
+            $this->errorsPost[] = 'Details is required';
+        }
+
+    }
+
+    //gets all post from database, 
+    //used for Post Action only
+    public static function getPost()
+    {
+        $sql = 'SELECT up.*, u.name FROM user_posts AS up INNER JOIN users AS u
+        WHERE up.user_id = u.id';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        //$stmt->bindValue('');
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+    
+    //used when update Action is called
+    public static function updatepost($data, $post_id)
+    {
+        //var_dump($data);
+        $type = $data['type'];
+        $title = $data['title'];
+        $details = $data['details'];
+        $sql = 'UPDATE user_posts 
+                SET catogery = :post_type,
+                    title = :post_title,
+                    discription = :post_discription
+                WHERE id = :post_id ';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        
+        $stmt->bindValue(':post_type', $type, PDO::PARAM_STR);
+        $stmt->bindValue(':post_title', $title, PDO::PARAM_STR);
+        $stmt->bindValue(':post_discription', $details, PDO::PARAM_STR);
+        $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+
+        if($stmt->execute())
+        {    
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //used when delete Action is called
+    public static function deletePost($post_id)
+    {
+        $sql = 'DELETE FROM user_posts WHERE id =:post_id ';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        
+        $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+    }
+
+    //used while edit Acion is called
+    public static function getUserPostById($id)
+    {
+        $user = Auth::getUser();
+        $sql = 'SELECT * FROM user_posts WHERE id =:post_id AND user_id = :user_id';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':post_id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':user_id', $user->id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    public static function isUserPost($id)
+    {
+        $userPost = static::getUserPostById($id);
+        if($userPost)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    } 
+
+    //used for profile viewing and many more
+    public static function getPostByUserId($id)
+    {
+        $sql = 'SELECT * FROM user_posts WHERE user_id =:user_id';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':user_id', $id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+}
